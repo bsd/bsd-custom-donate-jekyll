b9c712dc36e17e01461e5bc87832a796f79b7fe7
Drew Tipson <dtipson@gmail.com>
2013-12-12 12:56:55 -0500

    custom moma sequential

---
+++ webroot/page/-/donate/sequential-donate-moma.js
@@ -0,0 +1,15 @@
+---
+
+layout: none
+
+---
+
+(function($,w){
+
+{% include source/dsa-controller.js %}
+{% include source/jquery.detectCCType.js %}
+{% include source/jquery.bluecontribute.js %}
+{% include source/sequential-moma.js %}
+{% include source/default-custom-behavior.js %}
+
+}(jQuery,window));
